5716eff532cf28f24773dec67808b6f87a0b6d4a
Ron Appleton <ron.appleton@gmail.com>
2022-11-14 23:16:36 +0000

    Update consume.go

--- consume.go
+++ consume.go
@@ -3,35 +3,13 @@ package kafka
 import (
 	"context"
 	"github.com/Shopify/sarama"
-	"github.com/segmentio/kafka-go"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"sync"
 	"syscall"
-	"time"
 )
-
-func Consume(topic Topic, readDeadline time.Time) ([]byte, error) {
-	conn, err := kafka.DialLeader(context.Background(), "tcp", topic.Leader, topic.Topic, topic.Partition)
-	if err != nil {
-		log.Fatal("failed to dial leader:", err)
-	}
-
-	if err := conn.SetReadDeadline(readDeadline); err != nil {
-		log.Fatal("failed to set read deadline:", err)
-	}
-
-	msg, err := conn.ReadMessage(1e6) // fetch 1MB max
-	if err != nil {
-		log.Fatal("failed to read message:", err)
-
-		return []byte{}, nil
-	}
-
-	return msg.Value, nil
-}
 
 type messageHandler interface {
 	process(msg *sarama.ConsumerMessage)
