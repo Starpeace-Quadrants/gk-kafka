a3f3b30f167022908cd6af4210fc44af9fed8915
ronappleton <ron.appleton@gmail.com>
2022-11-17 22:41:33 +0000

    Update consume.go

--- consume.go
+++ consume.go
@@ -121,7 +121,7 @@ func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, clai
 		select {
 		case message := <-claim.Messages():
 			session.MarkMessage(message, "")
-			event.MustFire("messageReceived", event.M{"key": string(message.Key), "message": message})
+			event.MustFire("messageReceived", event.M{"key": string(message.Key), "message": *message})
 			log.Println("event fired to handle message")
 
 		case <-session.Context().Done():
